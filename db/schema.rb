f75dbee2834b456797fee798cbadd57b0fe65f64
Michael Boitin <mboitin@octo.com>
2019-05-03 14:47:00 +0200

    add image to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190430151028) do
+ActiveRecord::Schema.define(version: 20190503115723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20190430151028) do
     t.integer "expires_at"
     t.boolean "expires"
     t.string "refresh_token"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
