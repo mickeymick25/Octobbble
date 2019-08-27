b442c9d158f5dfdcc3ad1a1b6d307ecfcc4cbf1c
Eric Favre <efavre@octo.com>
2019-08-27 14:34:39 +0200

    correct indentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,22 +12,22 @@ class User < ApplicationRecord
     data = access_token.info
     user = User.where(email: data['email']).first
 
-  # Uncomment the section below if you want users to be created if they don't exist
-  unless user
-        password = Devise.friendly_token[0,20]
-        user = User.create(
-          name: data['name'],
-          email: data['email'],
-          image: data['image'],
-          password: password,
-          password_confirmation: password,
-          provider: access_token.provider,
-          uid: access_token.uid,
-          token: access_token.credentials.token,
-          expires: access_token.credentials.expires,
-          expires_at: access_token.credentials.expires_at,
-          refresh_token: access_token.credentials.refresh_token
-        )
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+      password = Devise.friendly_token[0,20]
+      user = User.create(
+        name: data['name'],
+        email: data['email'],
+        image: data['image'],
+        password: password,
+        password_confirmation: password,
+        provider: access_token.provider,
+        uid: access_token.uid,
+        token: access_token.credentials.token,
+        expires: access_token.credentials.expires,
+        expires_at: access_token.credentials.expires_at,
+        refresh_token: access_token.credentials.refresh_token
+      )
     end
     user
   end
