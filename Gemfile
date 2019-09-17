48b80df3ea1bc304f8e2b8b0cd27a5e5afa7008e
Eric Favre <efavre@octo.com>
2019-09-17 18:06:38 +0200

    test media type determination logic

--- Gemfile
+++ Gemfile
@@ -34,6 +34,10 @@ group :development, :test do
   gem 'selenium-webdriver'
 end
 
+group :test do
+  gem 'sqlite3'
+end
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
--- test/models/shot_test.rb
+++ test/models/shot_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ShotTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "shot with image png media should be image" do
+    assert shots(:image_shot).is_image? 
+  end
+
+  test "shot with video mpg media should not be image" do
+    assert ! shots(:video_shot).is_image? 
+  end
+
+  test "shot with video mpg media should be video" do
+    assert shots(:video_shot).is_video? 
+  end
 end
