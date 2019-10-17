3e17ab743781d09237b481f3caddc2cbf6e82d79
Eric Favre <efavre@octo.com>
2019-10-17 12:52:47 +0200

    add octopod client logs

--- app/services/story_creator.rb
+++ app/services/story_creator.rb
@@ -6,6 +6,7 @@ class StoryCreator
   end
 
   def initialize(octopod_id)
+    Rails.logger.info("initializing with #{octopod_id}")
     @octopod_id = octopod_id
   end
 
@@ -18,11 +19,14 @@ class StoryCreator
   end
 
   def initialize_from_octopod
+    Rails.logger.info("initializing with octopod")
     octopod_client = OctopodApi::V0::Client.new
     octopod_project = octopod_client.project(@octopod_id)
     if octopod_project
+      Rails.logger.info("Found octopod project #{octopod_project.name}")
       Project.new(title: octopod_project.name, description: octopod_project.description, clientname: octopod_project.customer)  
     else
+      Rails.logger.info("No matching octopod project")
       Project.new
     end
   end
